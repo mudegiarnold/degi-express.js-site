c86d8c91a5a46c091a143ff251d0f2dfb61ed4c1
mudegiarnold <136020298+mudegiarnold@users.noreply.github.com>
2024-01-05 20:29:30 +0000

    updated files

---
+++ src/jobs.js
@@ -0,0 +1,32 @@
+const JOBS = [
+    {
+        id: 1,
+        title: 'Frontend Developer',
+        location: 'Anywhere,Remote',
+        salary: '$100,000',
+        updated: 'Jan 10, 2023'
+    },
+    {
+        id: 2,
+        title: 'Full Stack Developer',
+        location: 'San Francisco,CA',
+        salary: '$120,000',
+        updated: 'Jan 15, 2023'
+    },
+    {
+        id: 3,
+        title: 'Data Scientist',
+        location: 'San Francisco,CA',
+        salary: '$150,000',
+        updated: 'Jan 20, 2023'
+    },
+    {
+        id: 4,
+        title: 'Machine Learning Engineer',
+        location: 'Remote',
+        salary: '$90,000',
+        updated: 'Jan 30, 2023'
+    }
+]
+
+module.exports = JOBS;
